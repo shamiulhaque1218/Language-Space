8ca796de18198ac2cf118947684520f20220c706
Shamiul haque <shamiulhaque1218@gmail.com>
2023-06-07 15:07:18 +0600

    logo done

--- Foreign_Language_Learning_School/src/App.jsx
+++ Foreign_Language_Learning_School/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="p-20">
          This is the first page of my website.
-         <img src="weblogo.png" className="rounded-full h-24 w-24" alt="" />
+         <img src="logo.png" className="rounded-full border-gray-500 border-2 h-24 w-24" alt="" />
          <Outlet> </Outlet>
     </div>
   )
