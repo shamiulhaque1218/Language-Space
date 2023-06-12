37c5809a495fda72bf7eb786a8b51696d115ff04
Shamiul haque <shamiulhaque1218@gmail.com>
2023-06-12 23:47:20 +0600

    add

--- Foreign_Language_Learning_School/src/Component/Home/ViewPopularClass.jsx
+++ Foreign_Language_Learning_School/src/Component/Home/ViewPopularClass.jsx
@@ -46,6 +46,14 @@ const ViewPopularClass = ({result}) => {
         }
       })
     }
+
+    fetch(`http://localhost:5000/jwt`, {
+        method:"POST",
+        headers: {
+            "content-type":"application/json",
+        }, body: JSON.stringify({username: user.email}),
+    }).then((res)=> res.json()).then((data) => localStorage.setItem("access-token", data?.token))
+
   }
   // *** post data end ***
     return (
